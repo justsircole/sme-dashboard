5c2633e9fb4dd6a608a0c78506c949f8a7e26bd8
justsircole <kimseakolo7.1@gmail.com>
2026-07-24 21:41:53 +0200

    which product sold the least

--- notebooks/explore.py
+++ notebooks/explore.py
@@ -8,3 +8,6 @@ print(df)
 
 revenue_by_branch = df.groupby("branch")["total_revenue"].sum()
 print(revenue_by_branch)
+
+quantity_by_product = df.groupby("product")["quantity_sold"].sum()
+print(quantity_by_product.sort_values())
